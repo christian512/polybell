8af8e1af006e5be1bf1533c939a08448a6218aff
chris2up9 <c.staufe@posteo.net>
2020-10-13 11:46:58 +0200

    utilities for larger calculations

---
+++ utils.py
@@ -0,0 +1,67 @@
+from itertools import product
+import numpy as np
+
+
+def get_deterministic_behaviors(inputs_a, inputs_b, outputs=[-1, 1]):
+    """
+    TODO: For speed up one could use sparse matrices or just integer implementation with binaries
+    Returns all deterministic behaviors corresponding to inputs for ALICE and BOB
+    This assumes a binary outcome
+    :param inputs_a: list of inputs for ALICE
+    :param inputs_b: list of inputs for BOB
+    :param outputs: possible outputs
+    :return: list of np.arrays
+    """
+    # check the number of outputs
+    assert len(outputs) == 2
+    # calculate dimensions
+    dim = len(inputs_a) * len(inputs_b) * (len(outputs) ** 2)
+    # define all hidden variables
+    lhvs = product(outputs, repeat=len(inputs_a) + len(inputs_b))
+    deterministics = []
+    for lhv in lhvs:
+        # counter for index within the behavior
+        counter = 0
+        # empty deterministic behavior
+        d = np.zeros(dim)
+        # iterate over the possible input and output combinations
+        for x, y in product(range(len(inputs_a)), range(len(inputs_b))):
+            for a, b in product(outputs, outputs):
+                if lhv[x] == a and lhv[y + len(inputs_a)] == b:
+                    d[counter] = 1.0
+                counter += 1
+        deterministics.append(d)
+    assert len(deterministics) == len(outputs) ** (len(inputs_a) + len(inputs_b))
+    return np.array(deterministics)
+
+
+def general_pr_box(a, b, x, y):
+    """
+    Calculates the PR box probability for any input number and binary outcome
+    :param a: Alices output
+    :param b: Bobs output
+    :param x: Alices input
+    :param y: Bobs input
+    :return: probability of PR box
+    """
+    # check the inputs
+    assert x >= 0
+    assert y >= 0
+    assert a == 1 or a == 0 or a == -1
+    assert b == 1 or b == 0 or b == -1
+    # if outputs are -1, replace them with 0 internally for modulo addition
+    if a == -1: a = 0
+    if b == -1: b = 0
+    # get binary arrays of the inputs
+    x_bin = np.fromiter(map(int, np.binary_repr(x)), dtype=int)
+    y_bin = np.fromiter(map(int, np.binary_repr(y)), dtype=int)
+    # check the length -> update if not the same length
+    if not len(x_bin) == len(y_bin):
+        l = max(len(x_bin), len(y_bin))
+        x_bin = np.fromiter(map(int, np.binary_repr(x, width=l)), dtype=int)
+        y_bin = np.fromiter(map(int, np.binary_repr(y, width=l)), dtype=int)
+    # multiply the two sides together
+    t = np.dot(x_bin, y_bin) % 2
+    # bianry sum of the outputs
+    s = (a + b) % 2
+    return 1 / 2 * int(s == t)
