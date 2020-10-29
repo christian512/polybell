d1e3fdacc5e3f8481debbeb6631a6ec90997ac43
chris2up9 <c.staufe@posteo.net>
2020-10-29 11:25:25 +0100

    fixed file name

--- parallel/facet_enum_eqdets_parallel.py
+++ parallel/facet_enum_eqdets_parallel.py
@@ -22,7 +22,7 @@ options = {"disp": False, "maxiter": 5000, "bland": True}
 method = 'simplex'
 
 # eq_dets_file
-eq_dets_file = '../data/equalizing_deterministics/4422_finished.txt'
+eq_dets_file = '../data/equalizing_deterministics/4422.txt'
 facets_file = '../data/facets/4422.txt'
 
 
