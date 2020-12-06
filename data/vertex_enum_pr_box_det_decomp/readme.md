f6ec73a6fa46865ec17a5d16c31bf7ee9a76ea44
chris2up9 <c.staufe@posteo.net>
2020-12-06 22:33:58 +0100

    add saving of bell inequalities

---
+++ data/vertex_enum_pr_box_det_decomp/readme.md
@@ -0,0 +1,4 @@
+This directory contains all bell inequalities that we found for a specific case, taking all deterministics that 
+contribute to the decomposition of the pr box at threshold efficiency. 
+
+If there are multiple decompositions possible, this also includes the inequalities that we found for all decompositions.
--- vertex_enum/pr_box_local_weight.py
+++ vertex_enum/pr_box_local_weight.py
@@ -2,11 +2,16 @@ from linearbell.utils import get_deterministic_behaviors, find_local_weight_prim
 from linearbell.lrs_helper import polyhedra_h_representation, run_lrs_h_repr, run_redund
 import numpy as np
 
+# number of inputs / outputs
+ma = 2
+mb = 2
+n = 3
+
 # set inputs / outputs
-inputs_a = range(3)
-inputs_b = range(3)
-outputs = range(3)
-outputs_wo_failure = range(2)
+inputs_a = range(ma)
+inputs_b = range(mb)
+outputs = range(n)
+outputs_wo_failure = range(n - 1)
 
 # get deterministic points
 dets = get_deterministic_behaviors(inputs_a, inputs_b, outputs)
@@ -17,43 +22,41 @@ eta = 4 / (4 + len(inputs_a))
 pr_box = [general_pr_box_extended(a, b, x, y, eta, outputs_wo_failure) for (a, b, x, y) in configs]
 pr_box = np.array(pr_box)
 
-rank = np.linalg.matrix_rank(dets)
-print('rank: {}'.format(rank))
 
-# choose independent deterministics
-ind_dets = [dets[0]]
-tmp_rank = np.linalg.matrix_rank(ind_dets)
-
-for d in dets[1:]:
-    tmp_dets = np.copy(np.array(ind_dets))
-    tmp_dets = np.r_[tmp_dets, [d]]
-    if np.linalg.matrix_rank(tmp_dets) > tmp_rank:
-        ind_dets.append(d)
-        tmp_rank = np.linalg.matrix_rank(tmp_dets)
-ind_dets = np.array(ind_dets)
-assert np.linalg.matrix_rank(ind_dets) == rank
-
-# get the local weight using these independent deterministic points
-weights = find_local_weight_primal(pr_box, dets)
-print('number of nonzero weights: {}'.format(len(weights[weights > 1e-6])))
-# set equalizing dets
-eq_dets = dets[weights > 1e-6]
-
-# write file for running the vertex enumeration
-lhs = np.copy(dets)
-lhs = np.r_[lhs, [pr_box]]
-rhs = np.ones(lhs.shape[0])
-lins = list(np.arange(weights.shape[0])[weights > 1e-6])
-lins.append(lhs.shape[0] - 1)
-
-repr = polyhedra_h_representation(lhs, rhs, lins, file='input.ine')
-
-# run redund
-run_redund('input.ine','input_redund.ine')
-
-# run the enumeration
-outfile = '../data/vertex_enum_pr_box/2233_local_weight_lins.ext'
-vertices, rays = run_lrs_h_repr('input_redund.ine', output_file=outfile, nproc=1)
+# load the possible combinations to build the PR box from file
+file = '../data/pr_box_contrib_det/{}{}{}{}.gz'.format(ma, mb, n, n)
+combinations = np.loadtxt(file)
+if len(combinations.shape) == 1:
+    # reshape to 2D array
+    combinations = combinations.reshape(1, combinations.shape[0])
 
 
+# check that the combination of deterministics rebuilds the pr box
+all_vertices = []
+for i in range(combinations.shape[0]):
+    c = combinations[i]
+    pr_test = np.sum(dets * c[:, np.newaxis], axis=0)
+    assert np.allclose(pr_test, pr_box)
 
+    # write file for running the vertex enumeration
+    lhs = np.copy(dets)
+    lhs = np.r_[lhs, [pr_box]]
+    rhs = np.ones(lhs.shape[0])
+    lins = list(np.arange(c.shape[0])[c > 1e-6])
+    lins.append(lhs.shape[0] - 1)
+
+    repr = polyhedra_h_representation(lhs, rhs, lins, file='input.ine')
+
+    # run redund
+    run_redund('input.ine', 'input_redund.ine')
+
+    # run the enumeration
+    outfile = 'out.ext'
+    vertices, rays = run_lrs_h_repr('input_redund.ine', output_file=outfile, nproc=1)
+    for v in vertices:
+        all_vertices.append(v)
+
+# store all vertices (bell inequalities we found)
+all_vertices = np.array(all_vertices)
+file = '../data/vertex_enum_pr_box_det_decomp/{}{}{}{}.gz'.format(ma, mb, n, n)
+np.savetxt(file, all_vertices)
