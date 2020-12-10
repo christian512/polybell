d0b911b7987eaa10636744b425186084a8160062
chris2up9 <c.staufe@posteo.net>
2020-12-10 11:57:04 +0100

    script for storing relabellings -> to save time for debugging

---
+++ data/relabels/readme.md
@@ -0,0 +1,4 @@
+contains all allowed relabellings for a given number of inputs and outputs
+Each entry corresponds to the index which has to be changed to this place.
+So if you have a bell expression B and one row of this file "relabel" then "B[relabel]" 
+is the relabelled version of B.
---
+++ facet_enum/store_relabels.py
@@ -0,0 +1,27 @@
+import argparse
+from linearbell.utils import get_deterministic_behaviors, get_allowed_relabellings, get_relabels_dets
+import numpy as np
+
+# Get arguments
+parser = argparse.ArgumentParser()
+parser.add_argument(dest='ma', help="number of inputs for ALICE")
+parser.add_argument(dest='mb', help='number of inputs for BOB')
+parser.add_argument(dest='n', help='number of outputs')
+args = parser.parse_args()
+ma = int(args.ma)
+mb = int(args.mb)
+n = int(args.n)
+
+# set inputs / outputs
+inputs_a = range(ma)
+inputs_b = range(mb)
+outputs = range(n)
+
+# get deterministic points
+dets = get_deterministic_behaviors(inputs_a, inputs_b, outputs)
+
+# get allowed relabellings
+allowed_relabellings = get_allowed_relabellings(inputs_a, inputs_b, outputs, outputs)
+# store the relabels
+file = '../data/relabels/{}{}{}{}.gz'.format(ma, mb, n, n)
+np.savetxt(file, allowed_relabellings)
