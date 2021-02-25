26ff66d9900bfdc19af597b4a330d1ce4c63c5f6
christian512 <c.staufe@posteo.net>
2021-02-26 00:39:23 +0100

    changes to polytope object

--- face_lattice_recursive/polytope.py
+++ face_lattice_recursive/polytope.py
@@ -1,9 +1,11 @@
 """ A class to describe a polytope """
 import numpy as np
+import subprocess
 import string
 import random
 from linearbell.panda_helper import run_panda_vertices_on_facet, read_inequalities, write_known_vertices
 from linearbell.adjacency_decomposition import furthest_vertex, rotate
+from linearbell.utils import equiv_check_adjacency_panda
 
 
 class Polytope():
@@ -20,46 +22,59 @@ class Polytope():
         self.parent = parent
         if not self.parent:
             self.parent == self
-        # Children of the polytope, which are faces with respect to some symmetry
-        self.children = []
         # dimensions
         self.dims = np.linalg.matrix_rank(self.deterministics) - 1
+        # empty faces
+        self.__faces = []
+        # empty classes
+        self.__classes = []
 
     def __str__(self):
         return "polytope {} with {} deterministics and {} children ".format(self.id, self.deterministics.shape[0],
                                                                             len(self.children))
 
-    def add_child(self, poly):
-        """ Adds a child as polytope """
-        # check if the deterministic points of child are subgroup of the ones that we have
-        if not self.check_valid_face(poly):
-            return False
-        if poly in self.children:
-            print('Child already stored')
-            return False
-        self.children.append(poly)
-        return True
+    def get_all_faces(self):
+        """ Finds all faces of the polytope """
+        if len(self.__faces) != 0:
+            return self.__faces
+        # Run panda to get faces
+        write_known_vertices(self.deterministics, file='knownvertices.ext')
+        cmd = 'panda_org knownvertices.ext -t 1 --method=dd > out.ine'
+        out = subprocess.run(cmd, shell=True)
+        inequalities = read_inequalities('out.ine').astype(float)
+        # generate polytopes from faces
+        for ineq in inequalities:
+            p = polytope_from_inequality(ineq, self)
+            self.__faces.append(p)
+        return self.__faces
 
-    def find_single_face(self):
+    def get_all_classes(self):
+        """ Finds all classes of faces of a polytope """
+        if len(self.__classes) != 0:
+            return self.__classes
+        if len(self.__faces) == 0:
+            self.get_all_faces()
+        # Reduce the faces to inequivalent ones
+        # TODO: Maybe a bit more advanced thing to choose the class representative?
+        self.__classes = [self.__faces[0]]
+        for i, f in enumerate(self.__faces):
+            equiv = False
+            for c in self.__classes:
+                if equiv_check_adjacency_panda(f.creating_face, c.creating_face, self.relabellings,
+                                               self.deterministics):
+                    equiv = True
+                    break
+            if not equiv:
+                self.__classes.append(f)
+        return self.__classes
+
+    def get_single_face(self):
         """ Finds a single face of the polytope, creates a new polytope and returns it """
         # run the modified version of panda
         write_known_vertices(self.deterministics, file='input.ext')
         run_panda_vertices_on_facet('input.ext', outfile='out.ine')
-        face = read_inequalities('out.ine')[0]
-        # get vertices that are on this facet
-        face_dets = np.array([v for v in self.deterministics if v @ face[:-1] == -1.0 * face[-1]])
-        sub_relabellings = []
-        for r in self.relabellings:
-            allowed = False
-            for sd in face_dets:
-                if np.any(np.sum((face_dets - sd[r]) ** 2, axis=1) < 1e-6):
-                    allowed = True
-                    break
-            if allowed:
-                sub_relabellings.append(r)
-        sub_relabellings = np.array(sub_relabellings)
-        # create new polytope
-        return Polytope(face_dets, sub_relabellings, creating_face=face, parent=self)
+        ineq = read_inequalities('out.ine').astype(float)[0]
+        return polytope_from_inequality(ineq, self)
 
     def rotate_polytope(self, ridge):
         """ Rotates the polytope around a child.This polytope is the face, and it's parent is the 'real' polytope """
@@ -70,30 +85,26 @@ class Polytope():
         # set the structure
         polytope = self.parent
         face = self
-        # get furthest vertex
+        # do rotation
         vertices = np.c_[polytope.deterministics, np.ones(polytope.deterministics.shape[0])]
         vertex = furthest_vertex(vertices, face.creating_face)
         new_face = rotate(vertices, vertex, face.creating_face, ridge.creating_face)
         # create a new polytope
-        face_dets = np.array([v for v in polytope.deterministics if v @ new_face[:-1] == -1.0 * new_face[-1]])
-        sub_relabellings = []
-        for r in polytope.relabellings:
-            allowed = False
-            for sd in face_dets:
-                if np.any(np.sum((face_dets - sd[r]) ** 2, axis=1) < 1e-6):
-                    allowed = True
-                    break
-            if allowed:
-                sub_relabellings.append(r)
-        sub_relabellings = np.array(sub_relabellings)
-        return Polytope(face_dets, sub_relabellings, creating_face=new_face, parent=polytope)
+        return polytope_from_inequality(new_face, polytope)
 
-    def check_valid_face(self, face):
-        """ Checks if a given face is valid for this polytope """
-        for det in face.deterministics:
-            equal = det == self.deterministics
-            equal = np.all(equal, axis=1)
-            if np.all(equal == False):
-                print('Not a valid face, due to deterministics')
-                return False
-        return True
+def polytope_from_inequality(ineq, poly):
+    """ Generates a polytope from an inequality """
+    sub_dets = np.array([v for v in poly.deterministics if v @ ineq[:-1] == -1.0 * ineq[-1]])
+    # find possible relabelling
+    sub_poss_relabellings = []
+    for r in poly.poss_relabellings:
+        allowed = False
+        for sd in sub_dets:
+            if np.any(np.sum((sub_dets - sd[r]) ** 2, axis=1) < 1e-6):
+                allowed = True
+                break
+        if allowed:
+            sub_poss_relabellings.append(r)
+    sub_poss_relabellings = np.array(sub_poss_relabellings)
+    # create new polytope
+    return Polytope(sub_dets, sub_poss_relabellings, creating_face=ineq, parent=poly)
