03a3042e40e74c813f10b536cb3469501e4c5fb5
christian512 <c.staufe@posteo.net>
2021-03-02 15:48:31 +0100

    more detailed description

--- face_lattice_recursive/rotation_around_all_relabellings.py
+++ face_lattice_recursive/rotation_around_all_relabellings.py
@@ -1,6 +1,5 @@
 """ Checks if the relabelling around the lowest polytope (and all it's relabellings, results in the other class) """
 
-
 from linearbell.utils import get_deterministic_behaviors, equiv_check_adjacency_panda
 import numpy as np
 from polytope import Polytope, polytope_from_inequality
@@ -17,51 +16,69 @@ relabels = np.loadtxt('../data/relabels/{}{}{}{}.gz'.format(2, 2, 2, 2)).astype(
 bell_polytope = Polytope(deterministics=dets, relabellings=relabels)
 all_polys[0] = [bell_polytope]
 
+
 def recursive_classes_lattice(level):
     """ Recursive construction of a classes face lattice """
     if len(all_polys[level]) == 0:
         return True
-    all_polys[level+1] = []
+    all_polys[level + 1] = []
     for p in all_polys[level]:
         if p.dims <= 0:
             continue
         # iterate through each face-class of this polytope
         for c in p.get_all_classes():
             # check if the class is equivalent under the bell polytope to already found polytopes
-            if not c.equiv_under_bell(all_polys[level+1]):
-                all_polys[level+1].append(c)
-    recursive_classes_lattice(level+1)
+            if not c.equiv_under_bell(all_polys[level + 1]):
+                all_polys[level + 1].append(c)
+    recursive_classes_lattice(level + 1)
 
 
+# build up the lattice
 recursive_classes_lattice(0)
 
-# take the first class from level 6
-f1 = all_polys[7][0]
-ridge = all_polys[8][0]
+# TODO: Get network graph
 
-# Since this is equivalent we want to see if we can rotate around a class representative that results in another class
-for relabel in f1.relabellings:
-    new_ineq = ridge.creating_face[relabel]
-    new_ineq = np.r_[new_ineq, ridge.creating_face[-1]]
-    # polytope from face
-    new_ridge = polytope_from_inequality(new_ineq, f1)
-    # rotate f1 around the new polytope
-    f2 = f1.rotate_polytope(new_ridge)
-    if np.all(f2.creating_face == 0):
-        continue
-    if not f1.equiv_under_parent(f2):
-        if not f1.equiv_under_bell(f2):
-            print('found new class under parent and bell')
-            if not f2.equiv_under_bell(all_polys[6]):
-                print('Found completly new class -> This case should not happen')
-        else:
-            print('equiv under bell')
-    else:
-        print('equiv under parent')
+# select faces and ridges to test
+faces = all_polys[7]
+ridges = all_polys[8]
+print('Number of faces: ', len(faces))
+print('number of ridges: ', len(ridges))
 
+# iterate through each face
+for i in range(len(faces)):
+    f1 = faces[i]
+    # indicator if a new face was found when rotating
+    found_new = False
+    # iterate through each ridge
+    for j in range(len(ridges)):
+        ridge = ridges[j]
+        # identifier string
+        prepend = 'face {}, ridge {}: '.format(i, j)
+        # check if the ridge is valid and get the correct polytope representation
+        new_ridge = f1.get_valid_face(ridge)
+        if not new_ridge:
+            print(prepend + 'Not a valid ridge')
+            continue
+        # set the ridge to be the valid ridge
+        ridge = new_ridge
+        # iterate through the relabellings of the face and rotate face around each relabelled version of the ridge
+        for relabel in f1.relabellings:
+            # relabel ridge
+            new_ineq = ridge.creating_face[relabel]
+            new_ineq = np.r_[new_ineq, ridge.creating_face[-1]]
+            new_ridge = polytope_from_inequality(new_ineq, f1)
+            new_ridge = f1.get_valid_face(new_ridge)
+            assert new_ridge
+            # rotate f1 around the new ridge
+            f2 = f1.rotate_polytope(new_ridge)
+            if np.all(f2.creating_face == 0):
+                print('all zeros')
+                continue
+            if not f1.equiv_under_parent(f2):
+                if not f1.equiv_under_bell(f2):
+                    found_new = True
+                    print(prepend + 'Found new face!')
+                    # TODO: Add edges to the Graph
+                    assert f2.equiv_under_bell(faces), 'Found completely new class'
 
-
-# plot = print_face_lattice(all_polys)
-# from bokeh.io import show, save
-# show(plot)
-
+# TODO: Draw the graph
