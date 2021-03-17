79c50d8b9d89a6edcfcdc70ff890765917e44420
christian512 <c.staufe@posteo.net>
2021-03-17 11:18:14 +0100

    test prada

--- prada_testing/test_prada.py
+++ prada_testing/test_prada.py
@@ -2,11 +2,12 @@ from linearbell.utils import get_deterministic_behaviors
 from linearbell.panda_helper import write_known_vertices, read_inequalities
 import subprocess
 import matplotlib.pyplot as plt
+import numpy as np
 import time
 
-input_output_configs = [(2, 2, 2, 2), (3, 3, 2, 2), (4, 3, 2, 2)]
-number_of_inequalities_to_find = [24, 684, 12480]
-recursion_levels_to_test = [0,1]
+input_output_configs = [(2, 2, 2, 2), (3,3,2,2), (4,3,2,2)]
+number_of_inequalities_to_find = [2, 3, 6]
+recursion_levels_to_test = [0]
 for config, num_ineqs in zip(input_output_configs, number_of_inequalities_to_find):
     times = []
     for recursion_level in recursion_levels_to_test:
@@ -14,7 +15,8 @@ for config, num_ineqs in zip(input_output_configs, number_of_inequalities_to_fin
         # same number of outputs
         assert config[2] == config[3]
         vertices = get_deterministic_behaviors(range(config[0]), range(config[1]), range(config[2]))
-        write_known_vertices(vertices, 'randa_test_file.ext')
+        relabels_dets = np.loadtxt('data/relabels_dets/{}{}{}{}.gz'.format(config[0], config[1], config[2], config[3])).astype(int)
+        write_known_vertices(vertices, 'randa_test_file.ext', relabellings_vertices=relabels_dets)
         # run randa
         cmd = 'randa randa_test_file.ext -t 1 -r ' + str(recursion_level) + ' > out.ine'
         out = subprocess.run(cmd, shell=True)
@@ -22,6 +24,7 @@ for config, num_ineqs in zip(input_output_configs, number_of_inequalities_to_fin
         times.append(time.time() - start_time)
         assert inequalities.shape[0] == num_ineqs, inequalities.shape[0]
     plt.scatter(recursion_levels_to_test, times, label=str(config))
+    
 plt.title('Speed comparison Recursive Panda w/o recalculation checks ')
 plt.xlabel('Recursion depth')
 plt.ylabel('Time (s)')
