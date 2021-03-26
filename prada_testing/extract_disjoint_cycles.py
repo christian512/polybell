5344f27e761301e9f52bf0aa06d0aba8ce930f80
christian512 <c.staufe@posteo.net>
2021-03-26 11:42:47 +0100

    change notation of disjoint cycles

--- prada_testing/extract_disjoint_cycles.py
+++ prada_testing/extract_disjoint_cycles.py
@@ -5,8 +5,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import time
 
-
-relabels_dets = np.loadtxt('../data/relabels_dets/{}{}{}{}.gz'.format(2, 2, 2, 2)).astype(int)
+relabels_dets = np.loadtxt('../data/relabels_dets/{}{}{}{}.gz'.format(4, 3, 2, 2)).astype(int)
+print('loaded relabels, extracting disjoint cycles')
 
 # storage for all cycles
 all_cycles = []
@@ -39,8 +39,25 @@ for relabel in relabels_dets:
 out = ""
 for cycles in all_cycles:
     for cycle in cycles:
+        out = out + "("
         for val in cycle:
-            out += str(val) + " "
-        out = out[:-1] + ","
-    out = out[:-1] + "\n"
+            out += str(val + 1) + ","
+        out = out[:-1] + ")"
+    out = out + ",\n"
+out = out[:-2]
+f = open('disjoint_cycles.ext', 'w+')
+f.write(out)
+f.close()
+print('Number of exported cycles: ', relabels_dets.shape[0])
+
+# store vertices for polytope
+dets = get_deterministic_behaviors(range(4), range(3), range(2))
+out = ""
+for d in dets:
+    out += str(list(d.astype(int))) + ",\n"
+out = out[:-2]
 print(out)
+f = open('vertices.ext', 'w+')
+f.write(out)
+f.close()
+print('Number of exported vertices: ', dets.shape[0])
