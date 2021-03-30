048c3c86e4676cc11025619982a1ab7356ebfe40
christian512 <c.staufe@posteo.net>
2021-03-30 12:22:41 +0200

    add conversion to disjoint cycles

--- gap_testing/generators_perm_group.py
+++ gap_testing/generators_perm_group.py
@@ -1,5 +1,5 @@
 from linearbell.utils import get_relabelling_generators, get_deterministic_behaviors, get_relabels_dets
-
+from linearbell.gap_helper import relabels_dets_to_disjoint_cycles
 ma, mb, na, nb = 4, 4, 2, 2
 
 inputs_a, inputs_b, outputs_a, outputs_b = range(ma), range(mb), range(na), range(nb)
@@ -7,4 +7,5 @@ inputs_a, inputs_b, outputs_a, outputs_b = range(ma), range(mb), range(na), rang
 generators = get_relabelling_generators(inputs_a, inputs_b, outputs_a, outputs_b)
 vertices = get_deterministic_behaviors(inputs_a, inputs_b, outputs_a)
 automorphisms = get_relabels_dets(vertices, generators, show_progress=1)
+disjoint_cylces = relabels_dets_to_disjoint_cycles(automorphisms)
 
--- linearbell/gap_helper.py
+++ linearbell/gap_helper.py
@@ -2,3 +2,40 @@
 
 def relabels_dets_to_disjoint_cycles(relabels_dets):
     """ Converts the relabellings on deterministic points to disjoint cycles """
+    all_cycles = []
+
+    for relabel in relabels_dets:
+        # convert to dict
+        d = {}
+        for i in range(relabel.shape[0]):
+            d[i] = relabel[i]
+        # generate cycles
+        cycles = []
+        while len(d) > 0:
+            cycle = []
+            key = list(d)[0]
+            val = d.pop(key)
+            if val != key:
+                cycle.append(key)
+                cycle.append(val)
+            while val in list(d):
+                key = val
+                val = d.pop(key)
+                if val not in cycle:
+                    cycle.append(val)
+            if cycle:
+                cycles.append(cycle)
+        if cycles:
+            all_cycles.append(cycles)
+
+    # Store the cycles
+    out = ""
+    for cycles in all_cycles:
+        for cycle in cycles:
+            out = out + "("
+            for val in cycle:
+                out += str(val + 1) + ","
+            out = out[:-1] + ")"
+        out = out + ",\n"
+    out = out[:-2]
+    return out
