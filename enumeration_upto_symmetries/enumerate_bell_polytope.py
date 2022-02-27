c10d96bfa375e3104aed69cfceebd0e4a977701f
christian512 <c.staufe@posteo.net>
2022-02-27 21:21:16 +0100

    update enumeration for DD method

--- enumeration_upto_symmetries/enumerate_bell_polytope.py
+++ enumeration_upto_symmetries/enumerate_bell_polytope.py
@@ -1,6 +1,8 @@
 """ Script for enumerating the facet-classes of a two-party Bell polytope """
 
 import argparse
+import sys
+
 from gap_program_templates import *
 from linearbell.utils import get_relabelling_generators, get_deterministic_behaviors_two_party, get_relabels_dets
 from linearbell.gap_helper import relabels_dets_to_disjoint_cycles
@@ -41,15 +43,41 @@ generators = get_relabelling_generators(inputs_a, inputs_b, outputs_a, outputs_b
 automorphisms = get_relabels_dets(vertices, generators, show_progress=0)
 disjoint_cycles = relabels_dets_to_disjoint_cycles(automorphisms)
 
+# +++ DD Method +++
+if args.dd_method:
+    outfile = '{}{}{}{}_dd.ine'.format(ma, mb, na, nb)
+    cmd = 'randa -t 1 -m dd ' + randa_filename + ' > ' + outfile
+    randa_proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
+    print('Started the RANDA program.')
+
+    # define behavior for receiving a SIGINT (e.g. pressing ctrl+c)
+    def handler(signum, frame):
+        print("\n Ctrl-c was pressed. Stopping GAP and RANDA.")
+        os.killpg(os.getpgid(randa_proc.pid), signal.SIGTERM)
+        os.killpg(os.getpgid(gap_proc.pid), signal.SIGTERM)
+        exit(1)
+    signal.signal(signal.SIGINT, handler)
+    # Read output from RANDA process
+    while randa_proc.poll() is None:
+        line = randa_proc.stdout.readline()
+        if line:
+            print(line)
+    print('The inequality classes were written to: ' + outfile)
+    randa_proc.wait()
+    sys.exit(0)
+
+
+# +++ AD METHOD +++
+
 # Identify the equivalence test to perform and select the template script accordingly
 script = ''
 if args.equiv_method == 'stabilizer':
     print('Generating GAP program with stabilizer equivalence test')
     script = template_stabilizer_equiv_test % disjoint_cycles
-if args.equiv_method == 'global':
+elif args.equiv_method == 'global':
     print('Generating GAP program with global equivalence test')
     script = template_global_equiv_test % disjoint_cycles
-if args.equiv_method == 'local':
+elif args.equiv_method == 'local':
     print('Generating GAP program with local equivalence test')
     script = template_local_equiv_test % disjoint_cycles
 gap_filename = 'gap_scripts/{}{}{}{}_{}.g'.format(ma, mb, na, nb, args.equiv_method)
@@ -82,8 +110,6 @@ outfile = '{}{}{}{}.ine'.format(ma, mb, na, nb)
 cmd = 'randa -t 1 -r ' + str(args.recursion_depth) + ' -p ' + str(
     sampling_flag) + ' ' + randa_filename + ' > ' + outfile
 
-if args.dd_method:
-    cmd = 'randa -t 1 -m dd ' + randa_filename + ' > ' + outfile
 randa_proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
 print('Started the RANDA program.')
 
@@ -105,15 +131,12 @@ while randa_proc.poll() is None:
         print(line)
 
 print('The inequality classes were written to: ' + outfile)
-if args.dd_method:
-    print('The results where generated using the DoubleDescription method only')
-elif args.sampling_method:
+if args.sampling_method:
     print('The results were generated using the sampling Adjacency Decomposition method')
 else:
-    print('The results were generated using the {} equivalence test in the AdjacencyDecomposition method'.format(args.equiv_method))
+    print('The results were generated using the {} equivalence test in the AdjacencyDecomposition method'.format(
+        args.equiv_method))
 
 # Wait for RANDA and GAP to finish
 randa_proc.wait()
 gap_proc.wait()
-
-
